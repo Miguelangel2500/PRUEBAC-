010f67b37f2064e89d1916247da88b715d6a86f4
Miguelangel2500 <55053313+Miguelangel2500@users.noreply.github.com>
2020-02-26 16:01:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!!!!!");
+            int variable = 8;
+            string variable2 = "pepe";
+            Console.WriteLine(variable);
+            variable2 = Console.ReadLine();
+            Console.WriteLine(variable2);
+
+
+        }
+    }
+}
